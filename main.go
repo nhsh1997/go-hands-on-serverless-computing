ebd48b65f5a96990e6749be5396aa53aee1c0cc3
Hoang Nguyen <nhsh1997@gmail.com>
2020-07-15 12:38:46 +0700

    update

---
+++ main.go
@@ -0,0 +1,18 @@
+package go_hands_on_serverless_computing
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	Username string
+}
+
+func handler(e Event) (string, error) {
+	return fmt.Sprintf("<h1>Hello %s from Lambda Go</h1>", e.Username), nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
